e276f25e0c63343a30b416fcbfe9c7ed86e0df8a
curreesan <115443503+curreesan@users.noreply.github.com>
2025-07-09 00:13:30 +0530

    Add colors functionality and track grid size to update grid for reset, and color mode change

--- index.js
+++ index.js
@@ -4,46 +4,89 @@ const createNewGridBtn = document.querySelector('.grid-create');
 const resetGridBtn = document.querySelector('.grid-reset');
 const gridColorModeBtn = document.querySelector('.grid-color-mode');
 
-let gridColorMode = '';
-
-
+let gridColorMode='single';
+let trackGridSize = 16;
+let progressiveColor = {r:250,g:128, b:114};
 
 //Event Listeners
 createNewGridBtn.addEventListener('click', createNewGrid);
 
 resetGridBtn.addEventListener('click', ()=> {
-    createGrid();
-})
-
-container.addEventListener('mouseover', function(e){
-    if (e.target.classList.contains('grid-cell')) {
-        const cell = e.target;
-        console.log(`Row : ${cell.dataset.row} Column : ${cell.dataset.column}`);
-        colorChange(cell);
-    }
+    createGrid(trackGridSize);
 });
 
-gridColorModeBtn.addEventListener('click', function(e){
-    // console.log(e.target);
+container.addEventListener('mouseover', changeColor);
+
+gridColorModeBtn.addEventListener('click', toggleColorMode);
+
+//Functions
+function toggleColorMode(e) {
+// console.log(e.target);
     
     if (e.target.dataset.colorType == 'single') {
         e.target.innerHTML = 'random';
-        e.target.dataset.colorType = 'random'; 
+        e.target.dataset.colorType = 'random';
+        createGrid(trackGridSize);
+
     } else if (e.target.dataset.colorType == 'random') {
         e.target.innerHTML = 'progressive darkening';
         e.target.dataset.colorType = 'progressive-darkening'
+        createGrid(trackGridSize);
+
     } else if (e.target.dataset.colorType == 'progressive-darkening') {
         e.target.innerHTML = 'single color';
         e.target.dataset.colorType = 'single';
+        createGrid(trackGridSize);
     }
     
-    let gridColorMode = e.target.dataset.colorType;
-    // console.log(gridColorMode)
-})
+    gridColorMode = e.target.dataset.colorType;
+    console.log(gridColorMode)
+}
 
-//Functions
-function colorChange(cell) {
-    cell.style.backgroundColor = 'black';
+function changeColor(e) {
+    if (e.target.classList.contains('grid-cell')) {
+        const cell = e.target;
+        console.log(`Row : ${cell.dataset.row} Column : ${cell.dataset.column}`);
+        console.log(gridColorMode)
+        if (gridColorMode == 'single') {
+            cell.style.backgroundColor = setColorBlack();
+        } else if (gridColorMode == 'random') {
+            cell.style.backgroundColor = setColorRandom();
+        } else if (gridColorMode == 'progressive-darkening') {
+            setColorProgressive(e.target);
+        }
+    }
+}
+
+function setColorBlack() {
+     return `rgb(0,0,0)`;
+}
+
+function setColorRandom() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r},${g},${b})`;
+}
+
+function generateRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return { r, g, b };
+}
+
+function setColorProgressive(cell) {
+    let shade = parseInt(cell.dataset.shade);
+    if (shade <10) {
+        shade++;
+        cell.dataset.shade = shade;
+        const opacity = shade /10;
+        cell.style.backgroundColor = `rgba(${progressiveColor.r},${progressiveColor.g},${progressiveColor.b}, ${1 - opacity})`;
+        console.log(shade, cell.style.backgroundColor)
+    }
+
 }
 function createNewGrid() {
     const newGridSize = Number(prompt("Choose grid size between 1-100"));
@@ -53,6 +96,8 @@ function createNewGrid() {
 
 function createGrid(size = 16) {
 
+    trackGridSize = size;
+
     container.innerHTML = '';
 
     const cellSize = 100 / size; 
@@ -60,6 +105,7 @@ function createGrid(size = 16) {
     for (let i = 0; i < size; i++) {
         for (let j= 0; j < size; j++) {
             const cell = document.createElement('div');
+            cell.dataset.shade = 0;
             cell.dataset.row = i+1;
             cell.dataset.column = j+1;
             cell.classList.add('grid-cell');
